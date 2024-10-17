e231f148efc8552110397f2d42255c4f3cce0d9d
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-17 13:03:12 +0200

    add: date configurable via RequestParam

--- src/main/java/nikomitk/personaldigitalagentmsspeisekarte/controller/SpeisekarteController.java
+++ src/main/java/nikomitk/personaldigitalagentmsspeisekarte/controller/SpeisekarteController.java
@@ -1,25 +1,32 @@
 package nikomitk.personaldigitalagentmsspeisekarte.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nikomitk.personaldigitalagentmsspeisekarte.Speisekarte;
 import nikomitk.personaldigitalagentmsspeisekarte.service.SpeisekarteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RequestMapping("/speisekarte")
+@Slf4j
 public class SpeisekarteController {
 
     private final SpeisekarteService speisekarteService;
 
+    /**
+     * Test
+     *
+     * @param datum in format yyyy-MM-dd
+     */
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Speisekarte getSpeisekarte() {
-        return speisekarteService.getSpeisekarte();
+    public Speisekarte getSpeisekarte(@RequestParam Optional<String> datum) {
+        return speisekarteService.getSpeisekarte(datum);
     }
 
 }
--- src/main/java/nikomitk/personaldigitalagentmsspeisekarte/service/SpeisekarteService.java
+++ src/main/java/nikomitk/personaldigitalagentmsspeisekarte/service/SpeisekarteService.java
@@ -10,6 +10,7 @@ import org.springframework.util.MultiValueMap;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -30,15 +31,16 @@ public class SpeisekarteService {
                 .toList();
     }
 
-    public Speisekarte getSpeisekarte() {
-        String today = LocalDate.now().toString();
+    public Speisekarte getSpeisekarte(Optional<String> datumParam) {
+        String datum = datumParam.orElse(LocalDate.now().toString());
+
         String startThisWeek = LocalDate.now().getDayOfWeek().getValue() <= 3 ? LocalDate.now().minusDays(LocalDate.now().getDayOfWeek().getValue()).toString() : LocalDate.now().plusDays(7 - LocalDate.now().getDayOfWeek().getValue()).toString();
         String startNextWeek = LocalDate.now().getDayOfWeek().getValue() <= 3 ? LocalDate.now().plusDays(7L - LocalDate.now().getDayOfWeek().getValue()).toString() : LocalDate.now().plusDays(14 - LocalDate.now().getDayOfWeek().getValue()).toString();
 
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.put("func", List.of("make_spl"));
         formData.put("locId", List.of("16"));
-        formData.put("date", List.of(today));
+        formData.put("date", List.of(datum));
         formData.put("lang", List.of("de"));
         formData.put("startThisWeek", List.of(startThisWeek));
         formData.put("startNextWeek", List.of(startNextWeek));
